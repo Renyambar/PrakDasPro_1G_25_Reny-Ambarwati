3dd57084b2a1db20ea2f77584581d6765c6ee509
Renyambar <ambarwatireny73@gmail.com>
2024-09-17 15:02:56 +0700

    P3_Tugas2

--- src/Pertemuan3/Tugas2.java
+++ src/Pertemuan3/Tugas2.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Tugas2 {
     public static void main(String[] args) {
         Scanner gaji = new Scanner(System.in);
-        double jamKerja, upahPerjam, gajiPokok, gajiBonus, gajiTotal;
-
+        int jamKerja, upahPerjam,gajiBonus, gajiPokok, gajiTotal;
+        float bonus = 10/100f, pajak = 5/100f;
         System.out.print("Masukkan jumlah jam kerja: ");
-        jamKerja = gaji.nextDouble();
+        jamKerja = gaji.nextInt();
         System.out.print("Masukkan jumlah upah per jam (Rp): ");
-        upahPerjam = gaji.nextDouble();
+        upahPerjam = gaji.nextInt();
         gajiPokok = jamKerja * upahPerjam;
-        gajiBonus = gajiPokok + (0.1 * gajiPokok);
-        gajiTotal = gajiBonus - (0.05 * gajiBonus);
+        gajiBonus = (int) ( gajiPokok + (bonus * gajiPokok));
+        gajiTotal = (int) ( gajiBonus - (pajak * gajiBonus));
 
         System.out.println("Total gaji bulanan karyawan:Rp " + gajiTotal);
     }
