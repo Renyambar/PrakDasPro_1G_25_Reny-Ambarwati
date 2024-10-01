24d0bf0ac89779525afba5531ae81807da26e0b9
Renyambar <ambarwatireny73@gmail.com>
2024-10-01 14:36:00 +0700

    P5_Percobaan2_PertanyaanNo6

---
+++ src/Pertemuan5/PemilihanOperator25.java
@@ -0,0 +1,31 @@
+package Pertemuan5;
+import java.util.Scanner;
+public class PemilihanOperator25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1, angka2, hasil = 0;
+        char operator;
+        System.out.print("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+ - * :)");
+        operator = sc.next().charAt(0);
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+            System.out.println("Hasil: " + hasil);
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+            System.out.println("Hasil: " + hasil);
+        } else {
+            System.out.println("Error: Operator yang dipilih tidak sesuai");
+        }
+ 
+    }
+}
