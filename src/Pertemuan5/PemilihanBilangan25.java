ba56c9d12c8345dfb40b9207e30adf79c35684e5
Renyambar <ambarwatireny73@gmail.com>
2024-10-01 12:55:25 +0700

    P5_Percobaan1_PertanyaanNo1

--- src/Pertemuan5/PemilihanBilangan25.java
+++ src/Pertemuan5/PemilihanBilangan25.java
@@ -5,11 +5,7 @@ public class PemilihanBilangan25 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {System.out.println("Angka "+ angka + " termasuk bilangan ganjil");}
+        String result = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.println(result);
     }
 }
