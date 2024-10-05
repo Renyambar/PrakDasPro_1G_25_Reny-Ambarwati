60d9c937148ab3648f853bd5e91450e08083d93a
Renyambar <ambarwatireny73@gmail.com>
2024-10-05 22:06:03 +0700

    P6_Percobaan1_PertanyaanNo3

--- src/Pertemuan6/Pemilihan2Percobaan125.java
+++ src/Pertemuan6/Pemilihan2Percobaan125.java
@@ -7,11 +7,12 @@ public class Pemilihan2Percobaan125 {
         System.out.print("Masukkan Tahun: ");
         tahun = input25.nextInt();
         
-        
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100 == 0) && (tahun % 400 == 0)) {
                 System.out.println("Tahun Kabisat");
-            }
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+               }
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
