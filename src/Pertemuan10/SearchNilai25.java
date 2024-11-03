6e6e6db3586acb0e7b83c1fff45b814b73235b36
Renyambar <ambarwatireny73@gmail.com>
2024-11-03 23:18:10 +0700

    P10_Percobaan4_PertanyaanNo4

--- src/Pertemuan10/SearchNilai25.java
+++ src/Pertemuan10/SearchNilai25.java
@@ -1,19 +1,31 @@
 package Pertemuan10;
-
+import java.util.Scanner;;
 public class SearchNilai25 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
         int hasil = 0;
-
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jmlElemen = sc.nextInt();
+        
+        int[] arrNilai = new int[jmlElemen];
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        if (hasil != 0) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);  
+        } else {
+            System.out.println("Nilai yang dicaari tidak ditemukan");
+        }
         System.out.println();
     }
 }
