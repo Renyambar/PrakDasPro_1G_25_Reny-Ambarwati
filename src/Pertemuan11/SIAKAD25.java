9e15a2a1da1b8d113ada64c3979af28ca37a962e
Renyambar <ambarwatireny73@gmail.com>
2024-11-10 22:44:22 +0700

    P11_Percobaan4_PertanyaanNo1

--- src/Pertemuan11/SIAKAD25.java
+++ src/Pertemuan11/SIAKAD25.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class SIAKAD25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jmlMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jmlMhs][jmlMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
@@ -14,19 +19,19 @@ public class SIAKAD25 {
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/jmlMatkul);
         }
 
         System.out.println("====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jmlMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jmlMhs; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata  Kuliah " + (j+1) + ": " + totalPerMatkul/4);
+            System.out.println("Mata  Kuliah " + (j+1) + ": " + totalPerMatkul/jmlMhs);
         }
     }
 }
