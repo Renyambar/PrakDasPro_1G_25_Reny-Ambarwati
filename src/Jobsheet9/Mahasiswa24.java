ca2c95897b8e5807a407918e6af62c13014aecdd
Renyambar <ambarwatireny73@gmail.com>
2025-05-04 20:48:21 +0700

    P9_Percobaan1

---
+++ src/Jobsheet9/Mahasiswa24.java
@@ -0,0 +1,20 @@
+package Jobsheet9;
+public class Mahasiswa24 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa24(){
+
+    }
+
+    Mahasiswa24(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+}
